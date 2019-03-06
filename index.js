56a5e03bfde27be0a81158dbde30e0d8e7b0ee0b
Tashby <tashby2714@gmail.com>
2019-03-06 14:22:20 -0500

    readme update

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var listOfCoins = process.argv[3]; // 'Quarter,25,Dime,10,...
 var coinNamesAndValues = listOfCoins.split(','); //['Quarter', '25', 'Dime', '10']
 var numberOfCoins = coinNamesAndValues.length / 2
 
-var coinNames = getEven(coinNamesAndValues);
-var coinValues = getOdd(coinNamesAndValues);
+var coinNames = getOdd(coinNamesAndValues);
+var coinValues = getEven(coinNamesAndValues);
 
 function printCombinations(money, coins, usages, coinIndex) {
    //Things to do
     if (money == 0) {
-      return coinValues;
+      return 1;
     }
 }
 
@@ -38,7 +38,7 @@ function getEven(arr) {
 
 //Things to do:
 // FIX ***TypeError: Cannot read property 'split' of undefined
-// 1. Add recursion
+// 1. Add recursion in printCombinations
 // 2. Add coinIndex increment
 // 3. Add usages[?] increment
 // 4. Add solution at money == 0
